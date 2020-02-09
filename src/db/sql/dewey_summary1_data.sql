6a492cb9f1f8a1ce6481f625dc2cf8b197459334
Roland Fuchs <roland@owlbox.com>
2020-02-09 16:31:15 +0200

    add summary 1, 2 and 000 for 3 dewey data pop

---
+++ src/db/sql/dewey_summary1_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO dewey_summary(summary_id, name)
+VALUES(000, 'Computer science, information & general works'),
+(100, 'Philosophy & psychology'),
+(200, 'Religion'),
+(300, 'Social sciences'),
+(400, 'Language'),
+(500, 'Pure science'),
+(600, 'Technology'),
+(700, 'Arts & recreation'),
+(800, 'Literature'),
+(900, 'History & geography')
