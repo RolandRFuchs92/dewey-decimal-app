1b1713373a8632f5a891799192d46ac5e4c2118f
Roland Fuchs <roland@owlbox.com>
2020-02-11 10:46:39 +0200

    Add comment

--- src/pages/student/Student.jsx
+++ src/pages/student/Student.jsx
@@ -2,28 +2,36 @@ import React, { useState, useEffect } from 'react';
 import { startCase, lowerCase } from 'lodash';
 import MUIDataTable from 'mui-datatables';
 
-import { getStudentColumnNames, getStudentData, hideStudent } from './Student.repo';
-import EditDeleteCol, {useAddButton} from 'utils/tableButtons';
+import {
+	getStudentColumnNames,
+	getStudentData,
+	hideStudent,
+} from './Student.repo';
+import EditDeleteCol, { useAddButton } from 'utils/tableButtons';
 import { useDialog } from 'utils/dialog';
-import { useAlert } from 'utils/snackbarAlerts'
+import { useAlert } from 'utils/snackbarAlerts';
 
 import StudentModal from './Student.modal';
 
-export default function () {
+//comment
+
+export default function() {
 	const [columns, setColumns] = useState([]);
 	const [options, setOptions] = useState({});
 	const [data, setData] = useState([]);
 	const [student, setStudent] = useState({});
-	const [isOpen, setIsOpen] =useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 	const dialog = useDialog();
 	const alert = useAlert();
 	let columnVar;
 
-	const handleEditAdd = (rowData) => {
-		const obj = Object.fromEntries(columnVar.map(({name}, index) => [name,rowData[index]]));
+	const handleEditAdd = rowData => {
+		const obj = Object.fromEntries(
+			columnVar.map(({ name }, index) => [name, rowData[index]]),
+		);
 		setStudent(obj);
 		setIsOpen(true);
-	}
+	};
 
 	const addButton = useAddButton(handleEditAdd);
 
@@ -33,14 +41,17 @@ export default function () {
 				name,
 				label: startCase(lowerCase(name)),
 			}));
-		
-			const cols = [...columnText, ...(EditDeleteCol(handleEditAdd, handleDeleteClick))];
+
+			const cols = [
+				...columnText,
+				...EditDeleteCol(handleEditAdd, handleDeleteClick),
+			];
 			columnVar = cols;
 			setColumns(cols);
 			resetStudentList();
 			setOptions({
 				selectableRows: 'none',
-				...(addButton)
+				...addButton,
 			});
 		})();
 	}, []);
@@ -49,35 +60,44 @@ export default function () {
 		const studentData = await getStudentData();
 		setData(studentData);
 		setIsOpen(false);
-	}
+	};
 	const handleClose = () => {
 		setIsOpen(false);
-	}
+	};
 
 	const handleDeleteClick = rowData => {
-		const selectedStudent = Object.fromEntries(columnVar.map(({name}, index) => [name,rowData[index]]));
-		dialog({ title: 'Are you sure?', description: `Really delete ${selectedStudent.first_name} ${selectedStudent.last_name}?`, handleYes: () => handleYesForDelete(selectedStudent) })
-	}
+		const selectedStudent = Object.fromEntries(
+			columnVar.map(({ name }, index) => [name, rowData[index]]),
+		);
+		dialog({
+			title: 'Are you sure?',
+			description: `Really delete ${selectedStudent.first_name} ${selectedStudent.last_name}?`,
+			handleYes: () => handleYesForDelete(selectedStudent),
+		});
+	};
 
-	const handleYesForDelete = async (student) => {
+	const handleYesForDelete = async student => {
 		const studentName = `${student.first_name} ${student.last_name}`;
 		try {
 			await hideStudent(student.student_id);
 			await resetStudentList();
 			alert.success(`Successfully deleted ${studentName}`);
-		} catch(e) {
+		} catch (e) {
 			alert.error(`There was an error deleteing ${studentName}.`);
 		}
-	}
+	};
 
-	return (<>
+	return (
+		<>
 			<MUIDataTable
 				options={options}
 				title={'Student List'}
 				data={data}
 				columns={columns}
 			></MUIDataTable>
-			<StudentModal {...({ isOpen, student, handleClose, reset: resetStudentList})}></StudentModal>
+			<StudentModal
+				{...{ isOpen, student, handleClose, reset: resetStudentList }}
+			></StudentModal>
 		</>
 	);
 }
