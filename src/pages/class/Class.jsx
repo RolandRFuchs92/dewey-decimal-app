0cc54a37d575da6de05736a5c8832a316843a035
Roland Fuchs <rolandrfuchs92@gmail.com>
2020-02-19 20:02:55 +0200

    class_id to id

--- src/pages/class/Class.jsx
+++ src/pages/class/Class.jsx
@@ -23,7 +23,7 @@ export default () => {
         return [
             {
                 name: 'class_id',
-                label: 'Class Id' 
+                label: 'Id' 
             },{
                 name: 'class_name',
                 label: 'Name' 
