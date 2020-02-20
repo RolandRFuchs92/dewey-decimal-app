e6b25261234beeadb48cd492c8881b978ea8841e
Roland Fuchs <rolandrfuchs92@gmail.com>
2020-02-20 07:35:37 +0200

    Fix print

--- src/pages/books/Book.barcode.jsx
+++ src/pages/books/Book.barcode.jsx
@@ -31,34 +31,39 @@ export default (props) => {
     return <Modal {...props}>
         <Grid container direction="row" justify="center">
             <Grid item container direction="column" spacing={1} sm={6}>
-                <Grid item onClick={() => handlePrint(0)}>
-                    <ReactToPrint content={() => comp.current} trigger={() => <div><PrintButton variant="contained" text='Barcode' ></PrintButton></div> }></ReactToPrint>
+                <Grid item>
+                    <ReactToPrint content={() => comp.current} trigger={() => <div><PrintButton variant="contained" text='Print' disabled={!showQr && !showBarcode}></PrintButton></div> }></ReactToPrint>
                 </Grid>
-                <Grid item onClick={() => handlePrint(1)}>
-                    <ReactToPrint content={() => comp.current} trigger={() => <div><PrintButton variant="outlined" text='Qr code' ></PrintButton></div> }></ReactToPrint>
-                </Grid> 
-                <Grid item onClick={() => handlePrint(2)}>
-                    <ReactToPrint content={() => comp.current} trigger={() => <div><PrintButton variant="outlined" text='Qr & Barcode' ></PrintButton></div> }></ReactToPrint>
+                <Grid item>
+                    <FormControlLabel control={<Switch checked={showBarcode} onChange={() => setBarcode(!showQr)} />}
+                        label="Barcode" labelPlacement="start"
+                    />
                 </Grid>
+                <Grid item>
+                    <FormControlLabel control={<Switch checked={showQr} onChange={() => setQr(!showQr)} />}
+                            label="QrCode" labelPlacement="start"
+                    />
+                </Grid>
+
             </Grid>
             <Grid item container direction="column" spacing={1} sm={6}>
-                <FormControlLabel control={<Switch checked={printDescription} onChange={() => setPrintDescription(!printDescription)} value="checkedA" />}
+                <FormControlLabel control={<Switch checked={printDescription} onChange={() => setPrintDescription(!printDescription)} />}
                     label="Hide description" labelPlacement="start"
                 />
             </Grid>
             <Grid item ref={comp}>
-                {showQr && <QrCode {...{value, description}}></QrCode>}
-                {showBarcode && <Barcode  {...{value, description}} ></Barcode>}
+                 <QrCode {...{value, description}} className={showQr ? '' : classes.hide}></QrCode>
+                <Barcode  {...{value, description}} className={showBarcode ? '' : classes.hide}></Barcode>
             </Grid>
         </Grid>
     </Modal>
 }
 
-const PrintButton = ({text, variant}) => {
+const PrintButton = ({text, variant, disabled}) => {
     return <Button
         variant={variant}
         color="primary"
-      
+        disabled={disabled}
         startIcon={Icons.Print}
       >
         {text}
