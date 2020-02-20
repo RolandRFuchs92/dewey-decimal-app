8337c78de35bea5256b366625f082bf4ff6a025e
Roland Fuchs <rolandrfuchs92@gmail.com>
2020-02-20 22:46:06 +0200

    add history tab to student profile

--- src/pages/student/StudentProfile.jsx
+++ src/pages/student/StudentProfile.jsx
@@ -39,78 +39,41 @@ const useStyles = makeStyles( theme => ({
         backgroundColor: 'white',
         marginLeft: 15,
         padding: 15
+    },
+    studentHistory: {
+        position:'absolute',
+        left: 0,
+        top: -65,
+        backgroundColor: theme.palette.primary.main,
+        cursor:'pointer',
+        color: 'white',
+        height:50,
+        boxShadow: 'black 1px 3px 4px',
+        width: 80,
+        display:'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 25,
+        borderTopLeftRadius: theme.shape.borderRadius,
+        borderTopRightRadius: theme.shape.borderRadius,
     }
 }));
 
 export default ({open, handleClose, studentId}) => {
+    const [isFront, setIsFront] = useState(true);
     const classes = useStyles();
     return <Modal {...{open, handleClose}} >
         <div className={classes.container}>
-            <div className={classes.studentHistory}>
-                {Icons.book}
+            <div className={classes.studentHistory} onClick={() => setIsFront(!isFront)}>
+                {Icons.History}
             </div>
             <Grid container className={classes.backgroundSetting}>
                 <div className={classes.profileDataContainer}>
-                    <Typography variant="h5">Nana Korengten Apigei John Bob Ross</Typography>
-                    <Divider></Divider>
-                    <Grid container alignContent="space-between" style={{height: '100%'}}>
-                        <Grid container item>
-                            <Grid item sm={6}>
-                                <Typography variant="body1">Birthday: 1 Jan 1992</Typography>
-                            </Grid>
-                            <Grid item sm={6}>
-                                <Typography variant="body1">Grade 1 Smart</Typography>
-                            </Grid>
-                        </Grid>
-
-                        <Grid container item sm={12}>
-                            <Grid item sm={12}>
-                                <Typography variant="h5">Mother</Typography>
-                            </Grid>
-                            <Grid item sm={6}>
-                                <Typography variant="body1">Sharon Fuchs</Typography>
-                            </Grid>
-                            <Grid item sm={6}>
-                                <Typography variant="body1">031 123 1234</Typography>
-                            </Grid>
-                            <Grid item sm={12}>
-                                <Typography variant="body1">sharon@somesortofbizmail.org.co.uk</Typography>
-                            </Grid>
-                        </Grid>
-                        <Grid container item sm={12}>
-                            <Grid item sm={12}>
-                                <Typography variant="h5">Father</Typography>
-                            </Grid>
-                            <Grid item sm={6}>
-                                <Typography variant="body1">John Fuchs</Typography>
-                            </Grid>
-                            <Grid item sm={6}>
-                                <Typography variant="body1">031 123 1234</Typography>
-                            </Grid>
-                            <Grid item sm={12}>
-                                <Typography variant="body1">john@somesortofbizmail.org.co.uk</Typography>
-                            </Grid>
-                        </Grid>
-
-                        <Grid container item sm={12}>
-                            <Grid item sm={12}>
-                                <Typography variant="h5">Next books due</Typography>
-                            </Grid>
-                            <Grid item sm={8}>
-                                <Typography variant="body1">The Magic Finger - Rahl Dahl</Typography>
-                            </Grid>
-                            <Grid item sm={4}>
-                                <Typography variant="body1" align="right">Due: 12 Feb 2020</Typography>
-                            </Grid>
-                            <Grid item sm={8}>
-                                <Typography variant="body1">The Twits - Rahl Dahl</Typography>
-                            </Grid>
-                            <Grid item sm={4}>
-                                <Typography variant="body1" align="right">Due: 12 Feb 2020</Typography>
-                            </Grid>
-                        </Grid>
-                        
-                    </Grid>
+                {
+                    isFront ? 
+                    <StudentCard></StudentCard> :
+                    <BooksHistory></BooksHistory>
+                }
                 </div>
                 <Grid container item justify="center" alignItems="center" className={classes.studentProfile}>
                     <div className={classes.studentPreImage}>
@@ -120,4 +83,75 @@ export default ({open, handleClose, studentId}) => {
             </Grid>
         </div>
     </Modal>
+}
+
+const BooksHistory = () => {
+    return <h1>HISTORY!!!</h1>
+}
+
+const StudentCard = () => {
+    const classes = useStyles();
+    return <Grid container alignContent="space-between" style={{height: '100%'}}>
+            <Grid item>
+                <Typography variant="h5">Nana Korengten Apigei John Bob Ross</Typography>
+                <Divider></Divider>
+            </Grid>
+            
+            <Grid container item>
+                <Grid item sm={6}>
+                    <Typography variant="body1">Birthday: 1 Jan 1992</Typography>
+                </Grid>
+                <Grid item sm={6}>
+                    <Typography variant="body1">Grade 1 Smart</Typography>
+                </Grid>
+            </Grid>
+
+            <Grid container item sm={12}>
+                <Grid item sm={12}>
+                    <Typography variant="h5">Mother</Typography>
+                </Grid>
+                <Grid item sm={6}>
+                    <Typography variant="body1">Sharon Fuchs</Typography>
+                </Grid>
+                <Grid item sm={6}>
+                    <Typography variant="body1">031 123 1234</Typography>
+                </Grid>
+                <Grid item sm={12}>
+                    <Typography variant="body1">sharon@somesortofbizmail.org.co.uk</Typography>
+                </Grid>
+            </Grid>
+            <Grid container item sm={12}>
+                <Grid item sm={12}>
+                    <Typography variant="h5">Father</Typography>
+                </Grid>
+                <Grid item sm={6}>
+                    <Typography variant="body1">John Fuchs</Typography>
+                </Grid>
+                <Grid item sm={6}>
+                    <Typography variant="body1">031 123 1234</Typography>
+                </Grid>
+                <Grid item sm={12}>
+                    <Typography variant="body1">john@somesortofbizmail.org.co.uk</Typography>
+                </Grid>
+            </Grid>
+
+            <Grid container item sm={12}>
+                <Grid item sm={12}>
+                    <Typography variant="h5">Next books due</Typography>
+                </Grid>
+                <Grid item sm={8}>
+                    <Typography variant="body1">The Magic Finger - Rahl Dahl</Typography>
+                </Grid>
+                <Grid item sm={4}>
+                    <Typography variant="body1" align="right">Due: 12 Feb 2020</Typography>
+                </Grid>
+                <Grid item sm={8}>
+                    <Typography variant="body1">The Twits - Rahl Dahl</Typography>
+                </Grid>
+                <Grid item sm={4}>
+                    <Typography variant="body1" align="right">Due: 12 Feb 2020</Typography>
+                </Grid>
+            </Grid>
+            
+        </Grid>
 }
