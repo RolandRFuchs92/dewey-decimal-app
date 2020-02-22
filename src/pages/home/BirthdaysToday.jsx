cc2912a38bee72750e9fe9dd14f606321c43f805
Roland Fuchs <rolandrfuchs92@gmail.com>
2020-02-22 13:26:55 +0200

    create birthdays

--- src/pages/home/BirthdaysToday.jsx
+++ src/pages/home/BirthdaysToday.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import {makeStyles, Typography, Paper} from '@material-ui/core';
+
 import Icons from 'components/icons';
+import { getBirthdays } from './Home.repo';
 
 const useStyles = makeStyles(theme => ({
     container: {
@@ -12,9 +13,22 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default () => {
+    const [state, setState] = useState([]);
     const classes = useStyles();
+    
+    useEffect(() => {
+        (async () => {
+            setState(await getBirthdays());
+        })();
+    },[])
+
 
     return <Paper className={classes.container}>
         <Typography variant="h6">{Icons.Birthday} Birthdays Today {Icons.Birthday}</Typography>
+        {state.map(({first_name, last_name, grade, class_name, teacher}, index) => {
+            return <div key={`${last_name}${class_name}${index}`}>
+                {first_name} {last_name} - {grade} {class_name} {teacher}
+            </div>
+        })}
     </Paper>
 }
--- src/pages/home/Home.repo.js
+++ src/pages/home/Home.repo.js
@@ -3,5 +3,5 @@ import { format } from 'date-fns';
 import { getStudentsWithBirthdays } from 'pages/students';
 
 export const getBirthdays = async () => {
-    return await getStudentsWithBirthdays();
+    return await getStudentsWithBirthdays(format(new Date(), 'YYYY-mm-dd'));
 }
