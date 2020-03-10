27cd8893632be02f644406161e1b9dd89e896b79
Roland Fuchs <rolandrfuchs92@gmail.com>
2020-03-10 18:42:51 +0200

    Implement all the stuff

--- src/components/layout/Layout.service.js
+++ src/components/layout/Layout.service.js
@@ -1,4 +1,3 @@
-import ERROR_COUNT from "pages/admin/errors/ErrorReport.constants";
 import { setError } from "pages/admin/errors/ErrorReport.action";
 import { errorLogErrorsCount } from "pages/admin/errors/ErrorReport.service";
 
