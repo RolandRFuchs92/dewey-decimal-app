198983bc7b49c7fb3ca16bc2367995452a9fa6cf
Roland Fuchs <rolandrfuchs92@gmail.com>
2020-03-15 01:10:44 +0200

    added compile data for quagga

---
+++ src/components/scanner/index.tsx
@@ -0,0 +1,88 @@
+import React, { useEffect } from "react";
+import Quagga from "quagga";
+import { makeStyles, Grid, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => {
+  return {
+    viewport: {
+      "& div.viewport canvas": {
+        display: "none"
+      },
+      "& div.viewport video": {
+        width: "100%"
+      },
+      "& .drawingBuffer": {
+        position: "absolute",
+        top: 0,
+        left: 0
+      }
+    },
+    videoContainer: {
+      padding: 10
+    }
+  };
+});
+
+type ScannerModel = {
+  onDetected: (r: { codeResult: { code: string } }) => void;
+  open: boolean;
+};
+
+export default ({
+  onDetected = (r: { codeResult: { code: string } }) =>
+    alert(r.codeResult.code),
+  open
+}: ScannerModel) => {
+  const classes = useStyles();
+
+  useEffect(() => {
+    if (open) {
+      Quagga.init(
+        {
+          inputStream: {
+            type: "LiveStream",
+            constraints: {
+              width: 640,
+              height: 480,
+              facing: "environment" // or user
+            }
+          },
+          locator: {
+            patchSize: "medium",
+            halfSample: true
+          },
+          numOfWorkers: 2,
+          readers: [
+            {
+              format: "code_128_reader"
+            }
+          ],
+          locate: true
+        },
+        function(err: Error) {
+          if (err) {
+            return console.log(err);
+          }
+          Quagga.start();
+        }
+      );
+      Quagga.onDetected(onDetected);
+
+      return () => Quagga.offDetected(onDetected);
+    }
+    return () => {};
+  }, [open]);
+
+  if (!open) return null;
+
+  return (
+    <Grid item md={6} className={classes.videoContainer}>
+      <div className={classes.viewport}>
+        <Typography variant="h5">
+          Display barcode to easily checkin/out
+        </Typography>
+        <div id="interactive" className="viewport"></div>
+      </div>
+    </Grid>
+  );
+};
