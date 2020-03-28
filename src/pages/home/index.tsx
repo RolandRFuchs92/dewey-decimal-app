7d620a6249aac5b0b3e1587262ad88b1ec0f44d7
Roland Fuchs <rolandrfuchs92@gmail.com>
2020-03-28 09:53:52 +0200

    Reintegrate overdue books tile

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -102,9 +102,9 @@ export const Home = ({ checkins, checkouts }: HomeProps) => {
         <ScansToday scans={checkins}></ScansToday>
       </HomePageTile>
 
-      {/* <HomePageTile title="Books Overdue" indicator={<OverdueIndicator />}>
+      <HomePageTile title="Books Overdue" indicator={<OverdueIndicator />}>
         <Overdue></Overdue>
-      </HomePageTile> */}
+      </HomePageTile>
 
       <HomePageTile title="Birthdays Today" indicator={<BirthdayIndicator />}>
         <BirthdaysToday></BirthdaysToday>
