5133e6e2c0fc1a1157103b909cf043503596b14c
Roland Fuchs <rolandrfuchs92@gmail.com>
2020-04-09 22:19:46 +0200

    Add unit tests for formbuttons

---
+++ src/components/buttons/FormButtons.test.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import FormButtons from './FormButtons';
+
+describe('<FormButtons .../>', () => {
+  const resetTxt = 'Reset';
+  const submitTxt = 'Submit';
+
+  const defaultOnReset = () => {};
+  const defaultOnSubmit = () => {};
+
+  function renderMethod(onReset = defaultOnReset, onSubmit = defaultOnSubmit) {
+    return render(<FormButtons onReset={onReset} onSubmit={onSubmit} />);
+  }
+
+  it('should render without crashing', () => {
+    renderMethod();
+  });
+
+  it('should render with text reset and submit', () => {
+    const container = renderMethod();
+    container.getByText(resetTxt);
+    container.getByText(submitTxt);
+  });
+
+  it('should call either button once if clicked', () => {
+    const resetMethod = jest.fn();
+    const submitMethod = jest.fn();
+    const container = renderMethod(resetMethod, submitMethod);
+    const resetButton = container.getByText(resetTxt);
+    const submitButton = container.getByText(submitTxt);
+
+    fireEvent.click(resetButton);
+    fireEvent.click(submitButton);
+
+    expect(resetMethod).toBeCalledTimes(1);
+    expect(submitMethod).toBeCalledTimes(1);
+  });
+});
