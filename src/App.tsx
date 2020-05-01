3ea94247a936d93929be848d950a0f72e4a0694e
Roland Fuchs <rolandrfuchs92@gmail.com>
2020-05-01 15:19:20 +0200

    Seperate redux store into its own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import { ConfirmProvider } from 'material-ui-confirm';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 import { Provider as GlobalProvider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
 
 import MainLayout from 'pages/layout';
 import ThemeProvider from 'components/theme';
 import { SnackbarProvider } from 'notistack';
-import reduxReducers from 'utils/redux/rootReducer';
 import { ErrorIndicator } from 'components/icons/Indicator';
 import Scan from 'pages/scan/Scan';
-
-const store = createStore(
-  reduxReducers,
-  composeWithDevTools(applyMiddleware(ReduxThunk))
-);
+import store from 'utils/redux/store';
 
 function App() {
   return (
--- src/pages/home/Home.service.ts
+++ src/pages/home/Home.service.ts
@@ -2,10 +2,12 @@ import { formatDate } from 'appSettings.json';
 import { ScansModel } from 'pages/booksOut/Booksout.type';
 import format from 'date-fns/format/index.js';
 import { ProcessedScansModel } from './Home.type';
+import store from 'utils/redux/store';
+import { setCheckinsToday, setCheckoutsToday } from './Home.action';
 
 export default () => {};
 
-export function processScanData(scans: ScansModel[]) {
+export function updateScansData(scans: ScansModel[]) {
   const today = format(new Date(), formatDate.from);
   const checkoutResults = scans
     ? scans.filter(x => x.check_out_date.toString() === today)
@@ -16,11 +18,16 @@ export function processScanData(scans: ScansModel[]) {
 
   const checkoutCount = checkoutResults.length;
   const checkinCount = checkinResults.length;
+
+  const checkoutCountAction = setCheckoutsToday(checkoutCount);
+  const checkinCountAction = setCheckinsToday(checkinCount);
+  store.dispatch(checkoutCountAction);
+  store.dispatch(checkinCountAction);
+
   const result: ProcessedScansModel = {
     checkoutResults,
     checkoutCount,
     checkinResults,
     checkinCount
   };
-  return result;
 }
