be2948b1d1ce93b4a2d58b300afad9d8cafefd81
Roland Fuchs <rolandrfuchs92@gmail.com>
2020-05-11 20:20:20 +0200

    Add end point to get dewey_summary_3 dropdown

--- src/pages/deweySystem/summary3/Summary3.controller.ts
+++ src/pages/deweySystem/summary3/Summary3.controller.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 
-import { Result } from 'types/generic.type';
+import { genericErrorHandle } from 'utils/httpHelpers/controller';
+import { Result, DropdownListModel } from 'types/generic.type';
 
-import summary3Repo from './Summary3.repo';
+import summary3Repo, { getSelectList } from './Summary3.repo';
 import { TableDeweySummary3Schema } from './Summary3.type';
+
+const handleErr = genericErrorHandle('Summary3');
 
 const router = express.Router();
 
@@ -13,6 +16,27 @@ router.get('/', async (req, res) => {
     result: summary3Result
   };
   res.send(result);
+});
+
+router.get('/dropdownlist', async (req, res) => {
+  try {
+    const decimalDropdownList = await getSelectList();
+    const result: Result<DropdownListModel[]> = {
+      result: decimalDropdownList
+    };
+    res.send(result);
+  } catch (error) {
+    handleErr(
+      '/',
+      error,
+      res,
+      `Error during a GET for the decimals dropdown List, params[${JSON.stringify(
+        req.body,
+        null,
+        2
+      )}]`
+    );
+  }
 });
 
 export default router;
--- src/pages/deweySystem/summary3/Summary3.service.ts
+++ src/pages/deweySystem/summary3/Summary3.service.ts
@@ -1,6 +1,17 @@
+import { dewey_summary_3 } from 'endpoints.json';
+import { GET } from 'utils/ajax/AjaxWrapper';
 import serviceBase from 'components/page/service.base';
 import { TableDeweySummary3Schema, DeweySummary3Schema } from './Summary3.type';
+import { DropdownListModel } from 'types/generic.type';
 
 export default serviceBase<TableDeweySummary3Schema, DeweySummary3Schema>(
   'dewey_summary_3'
 );
+
+export async function getSelectList() {
+  const result = await GET<{}, DropdownListModel[]>(
+    dewey_summary_3.dropdownList.uri,
+    {}
+  );
+  return result;
+}
