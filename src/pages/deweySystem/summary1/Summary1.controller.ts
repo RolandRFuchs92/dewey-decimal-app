74549569f65ff033dd4672944bdd2cfbbad3fabc
Roland Fuchs <rolandrfuchs92@gmail.com>
2020-05-13 20:17:29 +0200

    Get dewey_summary_1 done

--- src/pages/deweySystem/summary1/Summary1.controller.ts
+++ src/pages/deweySystem/summary1/Summary1.controller.ts
@@ -1,8 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { pick } from 'lodash';
+
 import summary1Repo, { getSelectList } from './Summary1.repo';
 import { genericErrorHandle } from 'utils/httpHelpers/controller';
 
-import { TableDeweySummarySchema } from './Summary1.type';
+import {
+  TableDeweySummarySchema,
+  DeweySummarySchemaArray
+} from './Summary1.type';
 import { Result, DropdownListModel } from 'types/generic.type';
 
 const router = express.Router();
@@ -15,6 +20,79 @@ router.get('/', async (req, res) => {
   };
 
   res.send(result);
+});
+
+async function addOrUpdateTeacherFunction(
+  req: Request,
+  res: Response,
+  method: 'PUT' | 'POST'
+) {
+  const decimalParams = pick(
+    req.body,
+    DeweySummarySchemaArray
+  ) as TableDeweySummarySchema;
+
+  try {
+    decimalParams.dewey_summary_id =
+      Number(decimalParams.dewey_summary_id) <= 0
+        ? undefined
+        : decimalParams.dewey_summary_id;
+
+    const bookResult = await summary1Repo.addOrUpdate(decimalParams);
+    const result: Result<boolean> = {
+      message: `Successfully ${bookResult} ${decimalParams.name}.`,
+      result: true
+    };
+    res.send(result);
+  } catch (error) {
+    handleErr(
+      '/',
+      error,
+      res,
+      `Error during a ${method} for a Dewey summary, params[${JSON.stringify(
+        req.body,
+        null,
+        2
+      )}]`
+    );
+  }
+}
+
+router.post('/', async (req, res) => {
+  addOrUpdateTeacherFunction(req, res, 'POST');
+});
+
+router.put('/', async (req, res) => {
+  addOrUpdateTeacherFunction(req, res, 'PUT');
+});
+
+router.delete('/', async (req, res) => {
+  const deleteObject = {
+    dewey_summary_id: req.body.id as number
+  };
+  try {
+    const deleteResult = await summary1Repo.deleteRow(
+      (deleteObject as unknown) as TableDeweySummarySchema
+    );
+    const result: Result<boolean> = {
+      message: deleteResult
+        ? 'Successfully deleted this Dewey summary reference.'
+        : 'There was an error deleting your Dewey summary reference.',
+      result: deleteResult
+    };
+    res.send(result);
+  } catch (error) {
+    handleErr(
+      '/',
+      error,
+      res,
+      `Error during a DELETE for a Dewey summary, params[${JSON.stringify(
+        req.body,
+        null,
+        2
+      )}]`
+    );
+  }
 });
 
 router.get('/dropdownlist', async (req, res) => {
--- src/pages/deweySystem/summary1/Summary1.type.ts
+++ src/pages/deweySystem/summary1/Summary1.type.ts
@@ -1,7 +1,13 @@
 export type DeweySummarySchema = {
-  dewey_summary_id: number;
+  dewey_summary_id?: number;
   summary_id: number;
   name: string;
 };
 
 export type TableDeweySummarySchema = DeweySummarySchema;
+
+export const DeweySummarySchemaArray: (keyof DeweySummarySchema)[] = [
+  'dewey_summary_id',
+  'name',
+  'summary_id'
+];
