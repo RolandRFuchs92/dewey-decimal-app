94c0d39130b4c3ac5709c073523e4cf5250ab14a
Roland Fuchs <rolandrfuchs92@gmail.com>
2020-05-13 21:11:12 +0200

    Get summary3 working

--- src/pages/deweySystem/summary3/Summary3.controller.ts
+++ src/pages/deweySystem/summary3/Summary3.controller.ts
@@ -2,13 +2,26 @@ import express from 'express';
 
 import { genericErrorHandle } from 'utils/httpHelpers/controller';
 import { Result, DropdownListModel } from 'types/generic.type';
+import {
+  deleteAction,
+  updateOrAddActionCreator
+} from 'utils/httpHelpers/genericCrud';
 
 import summary3Repo, { getSelectList } from './Summary3.repo';
-import { TableDeweySummary3Schema } from './Summary3.type';
+import {
+  TableDeweySummary3Schema,
+  DeweySummary3Schema,
+  deweySummary3SchemaArray
+} from './Summary3.type';
 
 const handleErr = genericErrorHandle('Summary3');
-
 const router = express.Router();
+const addOrUpdate = updateOrAddActionCreator<DeweySummary3Schema>(
+  deweySummary3SchemaArray,
+  'Summary3',
+  'dewey_summary_3_id',
+  summary3Repo.addOrUpdate
+);
 
 router.get('/', async (req, res) => {
   const summary3Result = await summary3Repo.getAll();
@@ -16,6 +29,24 @@ router.get('/', async (req, res) => {
     result: summary3Result
   };
   res.send(result);
+});
+
+router.post('/', async (req, res) => {
+  addOrUpdate(req, res, 'POST');
+});
+
+router.put('/', async (req, res) => {
+  addOrUpdate(req, res, 'PUT');
+});
+
+router.delete('/', async (req, res) => {
+  deleteAction<DeweySummary3Schema>(
+    req,
+    res,
+    'dewey_summary_3_id',
+    'DeweySummary3',
+    summary3Repo.deleteRow
+  );
 });
 
 router.get('/dropdownlist', async (req, res) => {
--- src/pages/deweySystem/summary3/Summary3.type.ts
+++ src/pages/deweySystem/summary3/Summary3.type.ts
@@ -10,3 +10,10 @@ export type DeweySummary3Schema = Omit<
   TableDeweySummary3Schema,
   'dewey_summary_2_name'
 >;
+
+export const deweySummary3SchemaArray: (keyof DeweySummary3Schema)[] = [
+  'dewey_summary_3_id',
+  'name',
+  'summary_2_id',
+  'summary_3_id'
+];
